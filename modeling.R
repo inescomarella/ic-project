acc471c62a157d161125337ebf7e6a158d76e911
inescomarella <inesmottacomarella@gmail.com>
2020-02-26 14:03:28 -0300

    Criei output dos gráficos e modifiquei os nomes dos outputs de tabeÃlas, falta testar

--- modeling.R
+++ modeling.R
@@ -27,10 +27,15 @@ Var <-
 # From now on the analyses is DIFFERENT to each species
 
 # 2.1. Importing species data -----
-cfm <- read_excel("./data/occu-7x1.xlsx",
-                  sheet = "Canis lupus familiaris")
+cfm <- read_excel("./data/occu-10x1.xlsx",
+                  sheet = "sp1")
 cfm <- cfm[, -1]
 View(cfm)
+
+# Metadata with species names
+sp_name <- read_excel("./data/species-names.xlsx")
+sp_name
+
 
 # 2.2. Final matrix to be read by unmarked - data table -----
 cfm.umf <- unmarkedFrameOccu(y = cfm, siteCovs = Var)
@@ -54,34 +59,23 @@ dec.list.cfm <-
     "psi(.)p(var)" = dec3.cfm
   )
 ms.dec.cfm <- modSel(dec.list.cfm)
-# zero output: detection models ----
-ms.dec.cfm   # Ordered by AIC
-write.table(
-  ms.dec.cfm,
-  file =
-    "./output/detection-models-10x1-llongicaudis.csv",
-  sep = ",",
-  row.names = TRUE,
-  col.names = NA
-)
-
+ms.dec.cfm   # Ordered by AIC --> COPY
 
 
 # 3.2. Intermediate step -----
 # If the detection model selected is influenced by the covariates "psi(.)p(var)", then it is necessary to disintegrate the function dec3 according to Akaike criterion
 dd.cfm <- dredge(dec3.cfm)
-# 1st output: covariate detection model  ----
-dd.cfm   # Ordered by AIC
+# Exporting covariate detection model  ----
+View(dd.cfm)   # Ordered by AIC
 write.table(
   dd.cfm,
-  file = "./output/detection-pVar-10x1-llongicaudis.csv",
+  file = "./output/detection-pVar-10x1-sp1.csv",
   sep = ",",
   row.names = TRUE,
   col.names = NA
 )
 
 table <- as.matrix(dd.cfm)
-
 importancia.var.cfm <- matrix(NA, nrow = 5, ncol = 6)
 rownames(importancia.var.cfm) <-
   c("p(Int)", "psi(Int)", "p(DsB_PLA)", "p(ele)",
@@ -101,127 +95,137 @@ for (i in 1:5) {
   importancia.var.cfm[i,] <-
     c(mean.t, sd.t)
 }
-importancia.var.cfm
-# 2nd output: covariates bias on detection ----
+View(importancia.var.cfm)
+
+# Exporting covariates bias on detection ----
 write.table(
   importancia.var.cfm,
-  file = "./output/detection-covariates-10x1-llongicaudis.csv",
+  file = "./output/detection-covariates-10x1-sp1.csv",
   sep = ",",
   row.names = TRUE,
   col.names = NA
 )
 
-# 3rd output: detection bias graphs ----
+# Exporting graph 1 ----
+png(
+  "figs/detection-covariates-10x1-sp1.png",
+  res = 300,
+  width = 2400,
+  height = 2200
+)
+par(mfrow = c(2, 2))
+op <-
+  par(
+    mfrow = c(2, 2),
+    mar = c(4.1, 3.1, 1, 1.1),
+    oma = c(0.5, 0.5, 4, 0.5),
+    xpd = NA
+  )
 
-{par(mfrow = c(2, 2))
-  op <-
-    par(
-      mfrow = c(2, 2),
-      mar = c(4.1, 3.1, 1, 1.1),
-      oma = c(0.5, 0.5, 4, 0.5),
-      xpd = NA
-    )
-
-  {plotCI(
-      x = 1:5,
-      y = importancia.var.cfm[, 1],
-      uiw = importancia.var.cfm[, 2],
-      yaxt = "n",
-      xaxt = "n",
-      ylab = "Coeficiente de regress?o",
-      xlab = NA,
-      mgp = c(2, 1, 0)
-    )
-    axis(side = 1,
-         at = seq(1, 5),
-         labels = FALSE)
-    axis(side = 2,
-         labels = TRUE,
-         cex.axis = 0.7,)
-    text(
-      x = seq(1, 5, by = 1),
-      par("usr")[3] - 0.25,
-      labels = rownames(importancia.var.cfm),
-      cex = 0.73,
-      srt = 25,
-      adj = c(0.8, 1.7)
-    )
-    }
-
-  {plotCI(
-      x = 1:5,
-      y = importancia.var.cfm[, 3],
-      uiw = importancia.var.cfm[, 4],
-      yaxt = "n",
-      xaxt = "n",
-      ylab = "delta AIC",
-      xlab = NA,
-      mgp = c(2, 1, 0)
-    )
-    axis(side = 1,
-         at = seq(1, 5),
-         labels = FALSE)
-    axis(side = 2,
-         labels = TRUE,
-         cex.axis = 0.7)
-    text(
-      x = seq(1, 5, by = 1),
-      par("usr")[3],
-      labels = rownames(importancia.var.cfm),
-      cex = 0.73,
-      srt = 25,
-      adj = c(0.8, 1.9)
-    )
+{
+  plotCI(
+    x = 1:5,
+    y = importancia.var.cfm[, 1],
+    uiw = importancia.var.cfm[, 2],
+    yaxt = "n",
+    xaxt = "n",
+    ylab = "Coeficiente de regressão",
+    xlab = NA,
+    mgp = c(2, 1, 0)
+  )
+  axis(side = 1,
+       at = seq(1, 5),
+       labels = FALSE)
+  axis(side = 2,
+       labels = TRUE,
+       cex.axis = 0.7,)
+  text(
+    x = seq(1, 5, by = 1),
+    par("usr")[3] - 0.25,
+    labels = rownames(importancia.var.cfm),
+    cex = 0.73,
+    srt = 25,
+    adj = c(0.8, 1.7)
+  )
   }
 
-  {plotCI(
-      x = 1:5,
-      y = importancia.var.cfm[, 5],
-      uiw = importancia.var.cfm[, 6],
-      yaxt = "n",
-      xaxt = "n",
-      ylab = "weight",
-      xlab = NA,
-      mgp = c(2, 1, 0)
-    )
-    axis(side = 1,
-         at = seq(1, 5),
-         labels = FALSE)
-    axis(side = 2,
-         labels = TRUE,
-         cex.axis = 0.7)
-    text(
-      x = seq(1, 5, by = 1),
-      par("usr")[3] - 0,
-      labels = rownames(importancia.var.cfm),
-      cex = 0.73,
-      srt = 25,
-      adj = c(0.8, 1.9)
-    )
-  }
-
-  binomnames.det <-
-    expression(bold(paste(
-      "Variáveis de detecção - ", italic("Eira barbara"), ""
-    )))
-  title(binomnames.det, line = 1, outer = TRUE)
+{
+  plotCI(
+    x = 1:5,
+    y = importancia.var.cfm[, 3],
+    uiw = importancia.var.cfm[, 4],
+    yaxt = "n",
+    xaxt = "n",
+    ylab = "delta AIC",
+    xlab = NA,
+    mgp = c(2, 1, 0)
+  )
+  axis(side = 1,
+       at = seq(1, 5),
+       labels = FALSE)
+  axis(side = 2,
+       labels = TRUE,
+       cex.axis = 0.7)
+  text(
+    x = seq(1, 5, by = 1),
+    par("usr")[3],
+    labels = rownames(importancia.var.cfm),
+    cex = 0.73,
+    srt = 25,
+    adj = c(0.8, 1.9)
+  )
 }
 
-#Just to remember the covariates: ~ ele+DistBorda_PLAN+RAI_Hum ~ RS1+RS2+RS3+RAI_Hum
+{
+  plotCI(
+    x = 1:5,
+    y = importancia.var.cfm[, 5],
+    uiw = importancia.var.cfm[, 6],
+    yaxt = "n",
+    xaxt = "n",
+    ylab = "weight",
+    xlab = NA,
+    mgp = c(2, 1, 0)
+  )
+  axis(side = 1,
+       at = seq(1, 5),
+       labels = FALSE)
+  axis(side = 2,
+       labels = TRUE,
+       cex.axis = 0.7)
+  text(
+    x = seq(1, 5, by = 1),
+    par("usr")[3] - 0,
+    labels = rownames(importancia.var.cfm),
+    cex = 0.73,
+    srt = 25,
+    adj = c(0.8, 1.9)
+  )
+}
+
+binomnames.det <-
+  expression(bold(paste(
+    "Variáveis de detecção - ", italic("Canis lupus familiaris"), ""
+  )))
+title(binomnames.det, line = 1, outer = TRUE)
+dev.off()
+
+#Just to remember the covariates: ~ ele+DistBorda_PLAN+RAI_Hum ~ 1
 
 # 3.3. Final detection model function -----
 # write here the final function ( ~ detection ~ occupancy), consider the occupancy null
 dec.sel.cfm <-
-  occu(~ 1 ~ 1, cfm.umf)
+  occu(~ ele + DistBorda_PLAN + RAI_Hum ~ 1, cfm.umf)
 
 det.cfm.pred <-
-  predict(dec.sel.cfm, type = "det", appendData = TRUE) # Detection values per site, it can be extrapolated to a differente data base
+  predict(dec.sel.cfm, type = "det", appendData = TRUE)
 colMeans(det.cfm.pred[, 1:4])
-# 4th output: detection bias per site ----
+
+# Exporting detection bias per site ----
 write.table(
   det.cfm.pred,
   file =
-    ".output/detection-persite-10x1-llongicaudis-p(.).csv",
+    "./output/detection-persite-10x1-sp1-p(var).csv",
   sep = ",",
   row.names = TRUE,
   col.names = NA
@@ -236,11 +240,12 @@ write.table(
 ocu.cfm <-
   occu(~ 1 ~ RS1 + RS2 + RS3 + RAI_Hum, cfm.umf)
 dd.ocu.cfm <- dredge(ocu.cfm)
-dd.ocu.cfm # Ordered by AIC
-# 5th output: occupancy models ----
+View(dd.ocu.cfm) # Ordered by AIC
+
+# Exporting occupancy models ----
 write.table(
   dd.ocu.cfm,
-  file = "./output/occupancy-models-10x1-llongicaudis.csv",
+  file = "./output/occupancy-psiVar-10x1-sp1.csv",
   sep = ",",
   row.names = TRUE,
   col.names = NA
@@ -248,7 +253,7 @@ write.table(
 
 
 # 4.2. Intermediate step -----
-# If more than one model is a explanatory function (according to AIC), then evaluate the models based on the mean and standard deviation
+# If more than one model is an explanatory function (according to AIC), then evaluate the models based on the mean and standard deviation
 
 table.ocu <- as.matrix(dd.ocu.cfm)
 OCU.importancia.var.cfm <-
@@ -272,111 +277,120 @@ for (i in 1:(ncol(table.ocu) - 5)) {
   OCU.importancia.var.cfm[i,] <-
     c(mean.t, sd.t)
 }
+View(OCU.importancia.var.cfm)
 
-OCU.importancia.var.cfm
-# 6th output: covariate influence in detection ----
+# Exporting covariate influence in detection ----
 write.table(
   OCU.importancia.var.cfm,
-  file = "./output/occupancy-pVar-10x1-ebarbara.cfm.csv",
+  file = "./output/occupancy-covariates-10x1-sp1.cfm.csv",
   sep = ",",
   row.names = TRUE,
   col.names = NA
 )
-# 7th output: occupancy graphs ----
-{par(mfrow = c(2, 2))
-  op <-
-    par(
-      mfrow = c(2, 2),
-      mar = c(4.1, 3.1, 1, 1.1),
-      oma = c(0, 0, 4, 0),
-      mgp = c(3, 1, 0),
-      xpd = NA
-    )
-
-  {plotCI(
-      x = 1:(ncol(table.ocu) - 5),
-      y = OCU.importancia.var.cfm[, 1],
-      uiw = OCU.importancia.var.cfm[, 2],
-      yaxt = "n",
-      xaxt = "n",
-      ylab = "Coeficiente de regress?o",
-      xlab = NA,
-      mgp = c(1.75, 0.2, 0)
-    )
-    axis(side = 1,
-         at = seq(1, (ncol(table.ocu) - 5)),
-         labels = FALSE)
-    axis(side = 2,
-         labels = TRUE,
-         cex.axis = 0.7)
-    text(
-      x = seq(1, (ncol(table.ocu) - 5), by = 1),
-      par("usr")[3] - 0.05,
-      labels = rownames(OCU.importancia.var.cfm),
-      cex = 0.73,
-      srt = 45,
-      adj = c(0.95, 1.5)
-    )
-    }
-
-  {plotCI(
-      x = 1:(ncol(table.ocu) - 5),
-      y = OCU.importancia.var.cfm[, 3],
-      uiw = OCU.importancia.var.cfm[, 4],
-      yaxt = "n",
-      xaxt = "n",
-      ylab = "Delta AIC",
-      xlab = NA,
-      mgp = c(1.75, 1, 0)
-    )
-    axis(side = 1,
-         at = seq(1, (ncol(table.ocu) - 5)),
-         labels = FALSE)
-    axis(side = 2,
-         labels = TRUE,
-         cex.axis = 0.7)
-    text(
-      x = seq(1, (ncol(table.ocu) - 5), by = 1),
-      par("usr")[3] - 0.01,
-      labels = rownames(OCU.importancia.var.cfm),
-      cex = 0.73,
-      srt = 45,
-      adj = c(0.95, 1.5)
-    )
-  }
-
-  {plotCI(
-      x = 1:(ncol(table.ocu) - 5),
-      y = OCU.importancia.var.cfm[, 5],
-      uiw = OCU.importancia.var.cfm[, 6],
-      yaxt = "n",
-      xaxt = "n",
-      ylab = "Weight",
-      xlab = NA,
-      mgp = c(1.75, 1, 0)
-    )
-    axis(side = 1,
-         at = seq(1, (ncol(table.ocu) - 5)),
-         labels = FALSE)
-    axis(side = 2,
-         labels = TRUE,
-         cex.axis = 0.7)
-    text(
-      x = seq(1, (ncol(table.ocu) - 5), by = 1),
-      par("usr")[3] - 0.02,
-      labels = rownames(OCU.importancia.var.cfm),
-      cex = 0.73,
-      srt = 45,
-      adj = c(0.9, 1.5)
-    )
-  }
-
-  binomnames.ocu <-
-    expression(bold(paste(
-      "Variáveis de ocupação - ", italic("Eira barbara"), ""
-    )))
-  title(binomnames.ocu, line = 1, outer = TRUE)
+# Exporting graph 2 ----
+png(
+  "figs/occupancy-covariates-10x1-sp1.png",
+  res = 300,
+  width = 2400,
+  height = 2200
+)
+par(mfrow = c(2, 2))
+op <-
+  par(
+    mfrow = c(2, 2),
+    mar = c(4.1, 3.1, 1, 1.1),
+    oma = c(0.5, 0.5, 4, 0.5),
+    xpd = NA
+  )
+{
+  plotCI(
+    x = 1:(ncol(table.ocu) - 5),
+    y = OCU.importancia.var.cfm[, 1],
+    uiw = OCU.importancia.var.cfm[, 2],
+    yaxt = "n",
+    xaxt = "n",
+    ylab = "Coeficiente de regressão",
+    xlab = NA,
+    mgp = c(1.75, 0.2, 0)
+  )
+  axis(side = 1,
+       at = seq(1, (ncol(table.ocu) - 5)),
+       labels = FALSE)
+  axis(side = 2,
+       labels = TRUE,
+       cex.axis = 0.7)
+  text(
+    x = seq(1, (ncol(table.ocu) - 5), by = 1),
+    par("usr")[3] - 0.05,
+    labels = rownames(OCU.importancia.var.cfm),
+    cex = 0.73,
+    srt = 45,
+    adj = c(0.95, 1.5)
+  )
 }
+
+{
+  plotCI(
+    x = 1:(ncol(table.ocu) - 5),
+    y = OCU.importancia.var.cfm[, 3],
+    uiw = OCU.importancia.var.cfm[, 4],
+    yaxt = "n",
+    xaxt = "n",
+    ylab = "Delta AIC",
+    xlab = NA,
+    mgp = c(1.75, 1, 0)
+  )
+  axis(side = 1,
+       at = seq(1, (ncol(table.ocu) - 5)),
+       labels = FALSE)
+  axis(side = 2,
+       labels = TRUE,
+       cex.axis = 0.7)
+  text(
+    x = seq(1, (ncol(table.ocu) - 5), by = 1),
+    par("usr")[3] - 0.01,
+    labels = rownames(OCU.importancia.var.cfm),
+    cex = 0.73,
+    srt = 45,
+    adj = c(0.95, 1.5)
+  )
+}
+
+{
+  plotCI(
+    x = 1:(ncol(table.ocu) - 5),
+    y = OCU.importancia.var.cfm[, 5],
+    uiw = OCU.importancia.var.cfm[, 6],
+    yaxt = "n",
+    xaxt = "n",
+    ylab = "Weight",
+    xlab = NA,
+    mgp = c(1.75, 1, 0)
+  )
+  axis(side = 1,
+       at = seq(1, (ncol(table.ocu) - 5)),
+       labels = FALSE)
+  axis(side = 2,
+       labels = TRUE,
+       cex.axis = 0.7)
+  text(
+    x = seq(1, (ncol(table.ocu) - 5), by = 1),
+    par("usr")[3] - 0.02,
+    labels = rownames(OCU.importancia.var.cfm),
+    cex = 0.73,
+    srt = 45,
+    adj = c(0.9, 1.5)
+  )
+}
+
+binomnames.ocu <-
+  expression(bold(paste(
+    "Variáveis de ocupação - ", italic("Canis lupus familiaris"), ""
+  )))
+title(binomnames.ocu, line = 1, outer = TRUE)
+dev.off()
+
+
 # Just to remember the covariates: ~ ele+DistBorda_PLAN+RAI_Hum ~ RS1+RS2+RS3+RAI_Hum
 
 # 4.3. Final occupancy model function -----
@@ -385,10 +399,11 @@ ocu.sel.cfm <-
   occu(~ 1 ~ RS3, cfm.umf)
 ocu.pred.cfm <- predict(ocu.sel.cfm, type = "state")
 colMeans(ocu.pred.cfm)
-# 8th output: occupancy per site ----
+View(ocu.pred.cfm)
+# Exporting occupancy per site ----
 write.table(
   ocu.pred.cfm,
-  file = "./output/occupancy-persite-10x1-llongicaudis-p(.)psi(RS3).csv",
+  file = "./output/occupancy-persite-10x1-sp1-p(.)psi(RS3).csv",
   sep = ",",
   row.names = TRUE,
   col.names = NA
