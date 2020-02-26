76903c85184bcf6a9c5341d306fb7bb892fac9b7
inescomarella <inesmottacomarella@gmail.com>
2020-02-26 11:39:17 -0300

    Script para padronizar os nomes das abas das planilhas de dados

---
+++ 0-prep-data.R
@@ -0,0 +1,73 @@
+# 2020-02-26 Inês Motta Comarella
+# changing the species names
+
+library(openxlsx)
+
+# Species code list, used to name the sheets
+sheet_names <-
+  c("sp1",
+    "sp2",
+    "sp3",
+    "sp4",
+    "sp5",
+    "sp6",
+    "sp7",
+    "sp8",
+    "sp9",
+    "sp10")
+#++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
+# Occasion 10x1 ----
+
+# Renaming the sheets
+data <- loadWorkbook("./data/occu-10x1.xlsx")
+for (i in 1:10) {
+  renameWorksheet(data, i, sheet_names[[i]])
+
+}
+
+# Saving the modifications
+saveWorkbook(data, "./data/occu-10x1.xlsx", overwrite = TRUE)
+
+#++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
+# Occasion 7x1 ----
+
+# Renaming the sheets
+data <- loadWorkbook("./data/occu-7x1.xlsx")
+for (i in 1:10) {
+  renameWorksheet(data, i, sheet_names[[i]])
+
+}
+
+# Saving the modifications
+saveWorkbook(data, "./data/occu-7x1.xlsx", overwrite = TRUE)
+
+#++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
+# Occasion 5x1 ----
+
+# Renaming the sheets
+data <- loadWorkbook("./data/occu-5x1.xlsx")
+for (i in 1:10) {
+  renameWorksheet(data, i, sheet_names[[i]])
+
+}
+
+# Saving the modifications
+saveWorkbook(data, "./data/occu-5x1.xlsx", overwrite = TRUE)
+
+
+#++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
+# Occasion 1x1 ----
+
+# Renaming the sheets
+data <- loadWorkbook("./data/occu-1x1.xlsx")
+for (i in 1:10) {
+  renameWorksheet(data, i, sheet_names[[i]])
+
+}
+
+# Saving the modifications
+saveWorkbook(data, "./data/occu-1x1.xlsx", overwrite = TRUE)
+
+
+
+
