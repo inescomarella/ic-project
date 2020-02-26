42a08bb8d5ad18c3c131a26ee957215e566b6cae
inescomarella <inesmottacomarella@gmail.com>
2020-02-26 09:41:45 -0300

    mudando a estratégia de manipulação dos dados

--- manipulating-data.R
+++ manipulating-data.R
@@ -8,40 +8,85 @@
 # I can analyse each occasion separately what will reduce my output to 40 (different species per occasion), but I was also curious about comparing among different occasion, in this case I'll add 10 outputs (different occasions per species)
 # First I'll consider one occasion, so I'll compare the species, then I'll compare the different occasions response in each species.
 
+#+++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
+# IMPORTANTE: versão atualizada do Java, com mesma arquitetura do R (no meu caso, x64 bit)
+# Após baixado rode o comando no terminal:
+setx PATH "C:/Program Files/Java/jre1.8.0_241/bin/server;%PATH%"
+# Em seguida no R, rode:
+Sys.setenv(JAVA_HOME = 'C:\\Program Files\\Java\\jre1.8.0_241')
+#+++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
+
+
 # packages necessary to run "xlsx" package
 install.packages(c("rJava", "xlsxjars", "xlsx"))
-
-
-
-setx PATH "C:/Program Files (x86)/Java/jre1.8.0_241/bin/server;%PATH%"
-Sys.setenv(JAVA_HOME = 'C:\\Program Files (x86)\\Java\\jre1.8.0_241')
 
 library(rJava)
 library(xlsxjars)
 library(xlsx)
 
-
+# Lendo os dados da primeira espécie para criar o arquivo
 det_models <-
-  read.csv(file = "./output/detection-models-10x1-llongicaudis.csv")
+  read.csv(file = "./output/detection-models-10x1-sp1.csv")
 det_pVar <-
-  read.csv(file = "./output/detection-pVar-10x1-llongicaudis.csv")
+  read.csv(file = "./output/detection-pVar-10x1-sp1.csv")
 det_covar <-
-  read.csv(file = "./output/detection-covariates-10x1-llongicaudis.csv")
+  read.csv(file = "./output/detection-covariates-10x1-sp1.csv")
 det_persite <-
-  read.csv(file = ".output/detection-persite-10x1-llongicaudis-p(.).csv")
+  read.csv(file = ".output/detection-persite-10x1-sp1-p(.).csv")
 occu_models <-
-  read.csv(file = "./output/occupancy-models-10x1-llongicaudis.csv")
+  read.csv(file = "./output/occupancy-models-10x1-sp1.csv")
 occu_pVar <-
-  read.csv(file = "./output/occupancy-pVar-10x1-ebarbara.cfm.csv")
+  read.csv(file = "./output/occupancy-pVar-10x1-sp1.cfm.csv")
 occu_persite <-
-  read.csv(file = "./output/occupancy-persite-10x1-llongicaudis-p(.)psi(RS3).csv")
+  read.csv(file = "./output/occupancy-persite-10x1-sp1-p(.)psi(RS3).csv")
+
+# Criando o arquivo e as páginas de acordo com os dados da primeira espécie
+xlsx.writeMultipleData <-
+  function ("./results/occupancy-model-OCCASION",
+            c(det_models,
+              det_pVar,
+              detcovar,
+              det_persite,
+              occu_models,
+              occu_pVar,
+              occu_persite))
+  {
+    require(xlsx, quietly = TRUE)
+    objects <-
+      list(det_models,
+           det_pVar,
+           detcovar,
+           det_persite,
+           occu_models,
+           occu_pVar,
+           occu_persite)
+    fargs <- as.list(match.call(expand.dots = TRUE))
+    objnames <- as.character(fargs)[-c(1, 2)]
+    nobjects <- length(objects)
+    for (i in 1:nobjects) {
+      if (i == 1)
+        write.xlsx(objects[[i]], file, sheetName = objnames[i])
+      else
+        write.xlsx(objects[[i]], file, sheetName = objnames[i],
+                   append = TRUE)
+    }
+  }
 
 
-write.xlsx2(
-  x,
-  file,
-  sheetName = "Sheet1",
-  col.names = TRUE,
-  row.names = TRUE,
-  append = FALSE
-)
+# Lendos os dados das outras espécies e adicionando às sheets
+# Para reconhecer o nome parcial dos arquivos
+det_models <- apropos("detection-models")
+n_det_models <- length(det_models)
+for (i in 1:n_det_models) {
+  if (i == 1)
+    write.x
+  lsx(det_models[[i]], file = "./results/occupancy-model-OCCASION", sheetName = objnames[i])
+  else
+    addDataFrame(det_model[[1]],
+                 det_models,
+                 startRow  =
+                 startColumn = 1)
+}
+
+
+####
